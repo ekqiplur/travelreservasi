4e166d7c6162f6bbd1e818feef0f4c3be0bf0f7b
Ekqiplur <ekqiplur@gmail.com>
2024-11-15 09:13:37 +0700

    add page bus

--- admin.php
+++ admin.php
@@ -4,13 +4,13 @@ include 'views/layout/admin/header.php';
     <!--*******************
         Preloader start
     ********************-->
-    <div id="preloader">
+    <!-- <div id="preloader">
         <div class="loader">
             <svg class="circular" viewBox="25 25 50 50">
                 <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
             </svg>
         </div>
-    </div>
+    </div> -->
     <!--*******************
         Preloader end
     ********************-->
